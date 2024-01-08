0366765471d11241b574804b9fc496c6b0855d1e
muhzin sidiq <muhzinsidhiq333@gmail.com>
2024-01-08 17:09:50 +0530

    fix the bug

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import "dotenv/config";
 import connectToDb from './config/connection.js';
 import userRouter from "./router/userRouter.js";
 import  adminRouer  from './router/adminRouter.js';
+import path from 'path'
 
 const app=express()
 const PORT = process.env.PORT
@@ -12,6 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, "../client/dist"))); 
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../client/dist/index.html'))
+})
 
 /*user route*/
 app.use("/user", userRouter);
