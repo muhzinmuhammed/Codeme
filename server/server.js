92f8652d4b2544af460032f64bd070c92571540d
muhzin sidiq <muhzinsidhiq333@gmail.com>
2024-01-08 20:11:53 +0530

    dist file

--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ import "dotenv/config";
 import connectToDb from './config/connection.js';
 import userRouter from "./router/userRouter.js";
 import  adminRouer  from './router/adminRouter.js';
-import path from 'path'
+
+import { fileURLToPath } from 'url';
+import path,{ dirname } from 'path';
 
 const app=express()
 const PORT = process.env.PORT
@@ -13,6 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('tiny'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log(__dirname,"ll");
 app.use(express.static(path.join(__dirname, "../client/dist"))); 
 app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'../client/dist/index.html'))
